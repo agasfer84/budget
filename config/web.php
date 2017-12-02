16aa15a629c43fa2931507bd60b00dd349c64943
agasfer84 <agasfer84@inbox.ru>
2017-12-02 21:13:02 +0400

    debug prod

--- config/web.php
+++ config/web.php
@@ -68,7 +68,7 @@ $config = [
     'params' => $params,
 ];
 
-if (YII_ENV_DEV) {
+if (YII_ENV_DEV||!YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
