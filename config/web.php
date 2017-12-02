565e465112bfdd4e16a67189ec68c2e3d96f249c
agasfer84 <agasfer84@inbox.ru>
2017-12-02 21:19:53 +0400

    debug prod 2

--- config/web.php
+++ config/web.php
@@ -68,13 +68,14 @@ $config = [
     'params' => $params,
 ];
 
-if (YII_ENV_DEV||!YII_ENV_DEV) {
+if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
         //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['127.0.0.1', '::1', '145.14.145.208'],
     ];
 
     $config['bootstrap'][] = 'gii';
@@ -82,6 +83,7 @@ if (YII_ENV_DEV||!YII_ENV_DEV) {
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
         //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['127.0.0.1', '::1', '145.14.145.208'],
     ];
 }
 
